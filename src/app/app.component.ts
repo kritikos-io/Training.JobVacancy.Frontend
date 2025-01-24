675f5a46ba7a5a8999c0b9fb35e7a36912bcd7a5
Konstantinos Petrogiannopoulos <konpetrogian@gmail.com>
2025-01-24 17:46:38 +0200

    first navbar implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { PlaygroundComponent } from "./playground/playground.component";
+
 @Component({
   selector: 'aa-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PlaygroundComponent],
   template: `
+    <aa-playground />
+
     <h1 class="aa--text-2xl">Welcome to {{ title }}!</h1>
 
     <router-outlet />
