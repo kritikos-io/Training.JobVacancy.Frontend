4c52d0c1c7dcb5c289ca71074fe876cdeb2eb172
kondritsas <154312323+kondritsas@users.noreply.github.com>
2025-01-24 19:09:49 +0200

    Create the icon component for social media icons

---
+++ src/app/playground/icon/icon.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faFacebook,
+  faYoutube,
+  faInstagram,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'aa-icon',
+  templateUrl: './icon.component.html',
+  styleUrl: './icon.component.css',
+  imports: [FontAwesomeModule],
+})
+export class IconComponent {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFacebook, faYoutube, faInstagram, faTwitter);
+  }
+}
