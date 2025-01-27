d02f655f4183842a596efeacfa1a7d8fd4334dd7
konpetrogian <156423047+konpetrogian@users.noreply.github.com>
2025-01-27 18:49:33 +0200

    responsivness+'burger menu' implementation

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './playground.component.html',
 })
 export class PlaygroundComponent {
-
+  classContent =
+    'aa--justify-left aa--flex aa--hidden aa--grow aa--flex-col aa--gap-5 aa--text-gray-500 md:aa--flex md:aa--flex-row';
+  onClick() {
+    if (
+      this.classContent ==
+      'aa--justify-left aa--flex aa--inline-block aa--grow aa--flex-col aa--gap-5 aa--text-gray-500 md:aa--flex md:aa--flex-row'
+    ) {
+      this.classContent =
+        'aa--justify-left aa--flex aa--hidden aa--grow aa--flex-col aa--gap-5 aa--text-gray-500 md:aa--flex md:aa--flex-row';
+    } else {
+      this.classContent =
+        'aa--justify-left aa--flex aa--inline-block aa--grow aa--flex-col aa--gap-5 aa--text-gray-500 md:aa--flex md:aa--flex-row';
+    }
+  }
 }
