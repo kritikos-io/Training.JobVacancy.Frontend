03f325ac5aad910b58db985ce6276c84634750ee
konpetrogian <96728487+Costaknc@users.noreply.github.com>
2025-01-31 17:23:34 +0200

    fixed_mobile_menu
    Fixed some icons. Fixed the menu to be created with ngFor/ngIf etc

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,28 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { NgFor, NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'aa-navbar',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, NgFor, NgIf, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   faBars = faBars;
+  faArrowUp = faArrowUp;
+  isMenuOpen = false;
 
-  classContent =
-    'aa--justify-left aa--flex aa--hidden aa--grow aa--flex-col aa--gap-5 aa--text-gray-500 md:aa--flex md:aa--flex-row';
-  onClick() {
-    if (
-      this.classContent ==
-      'aa--justify-left aa--flex aa--inline-block aa--grow aa--flex-col aa--gap-5 aa--text-gray-500 md:aa--flex md:aa--flex-row'
-    ) {
-      this.classContent =
-        'aa--justify-left aa--flex aa--hidden aa--grow aa--flex-col aa--gap-5 aa--text-gray-500 md:aa--flex md:aa--flex-row';
-    } else {
-      this.classContent =
-        'aa--justify-left aa--flex aa--inline-block aa--grow aa--flex-col aa--gap-5 aa--text-gray-500 md:aa--flex md:aa--flex-row';
+  navClass = 'aa--hidden';
+
+  MenuItems = [
+    { title: 'Home', href: '#', active: true },
+    { title: 'Find Job', href: '#', active: false },
+    { title: 'Employers', href: '#', active: false },
+    { title: 'Candidates', href: '#', active: false },
+    { title: 'Pricing Plans', href: '#', active: false },
+    { title: 'Customer Support', href: '#', active: false },
+  ];
+
+  trackByTitle(index: number, item: any): string {
+    return item.title;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    const windowWidth = (event.target as Window).innerWidth;
+    if (windowWidth >= 768 && this.isMenuOpen) {
+      this.isMenuOpen = false;
     }
+  }
+
+  handleMenu(): void {
+    if (this.isMenuOpen) {
+      this.navClass = '';
+    } else {
+      this.navClass = 'aa--hidden';
+    }
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.handleMenu();
   }
 }
