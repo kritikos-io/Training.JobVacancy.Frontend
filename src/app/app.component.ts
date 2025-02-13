608fa94fde6f8ba60dbc5ff806864983de600ed3
Vassilis Kontopanos <vasko77@yahoo.gr>
2025-02-13 11:33:14 +0200

    Rebased from latest Development branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { FooterComponent } from './shared/ui/footer';
 import { HeaderComponent } from './shared/ui/header';
+import { ToolbarComponent } from './shared/ui/header/toolbar';
 
 @Component({
   selector: 'aa-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, ToolbarComponent],
   template: `
     <aa-header />
-    <aa-toolbar />
     <h1 class="aa:text-2xl">Welcome to {{ title }}!</h1>
     <br />
     <div class="aa:flex aa:justify-center">
