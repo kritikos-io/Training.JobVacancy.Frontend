2c877b4bc85aa55e6718866f9f57245a6378981b
Konstantinos Petrogiannopoulos <konpetrogian@gmail.com>
2025-02-25 14:01:11 +0200

    pipe guard implementation

--- src/app/core/config/auth.guard.ts
+++ src/app/core/config/auth.guard.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { map, Observable } from 'rxjs';
 
 import { UserRole } from '../models/role.model';
 
@@ -15,17 +16,16 @@ export class AuthGuard implements CanActivate {
     private router: Router
   ) {}
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
-    if (this.auth.isAuthenticated()) {
-      const requiredRoles = route.data['role'] as UserRole[];
-      if (!this.authService.hasAccess(requiredRoles)) {
-        this.router.navigate(['/unauthorized']);
-        return false;
-      }
-      return true;
-    } else {
-      this.router.navigate(['/unauthorized']);
-      return false;
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    return this.auth.getAccessToken().pipe(
+      map(token => {
+        const requiredRoles = route.data['role'] as UserRole[];
+        const hasAccess = token && this.authService.hasAccess(requiredRoles);
+        if (!hasAccess) {
+          this.router.navigate(['/unauthorized']);
+        }
+        return token ? Boolean(this.authService.hasAccess(requiredRoles)) : false;
+      })
+    );
     }
   }
-}
