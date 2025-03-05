8b32b8ce422d41c08073f61b1269b290d15382c4
Konstantinos Petrogiannopoulos <konpetrogian@gmail.com>
2025-03-05 13:54:33 +0200

    OnInit impl, replaced constructor with 'inject()', some optimizations

--- src/app/features/company-create/company-create.component.ts
+++ src/app/features/company-create/company-create.component.ts
@@ -1,61 +1,68 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { CompanyApiService } from '../../core/config/company-api.service';
 import { CompanyModel } from '../../core/models/company.model';
 
 @Component({
   selector: 'aa-company-create',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, NgIf],
   templateUrl: './company-create.component.html',
 })
-export class CompanyCreateComponent {
+export class CompanyCreateComponent implements OnInit{
+  private fb = inject(FormBuilder);
+  private companyApi = inject(CompanyApiService);
+  
   company!: CompanyModel;
+  newCompany!: FormGroup;
 
-  newCompany = new FormGroup({
-    name: new FormControl('', Validators.required),
-    website: new FormControl('', [
-      Validators.pattern(/^(https?:\/\/)?[\w-]+(\.[\w-]+)+[/#?]?.*$/),
-    ]),
-    vat: new FormControl('', Validators.required),
-    logoUrl: new FormControl(''),
-    address: new FormGroup({
-      country: new FormControl(''),
-      city: new FormControl(''),
-      street: new FormControl(''),
-      streetNumber: new FormControl(''),
-      postalCode: new FormControl(''),
-    }),
-    isSponsored: new FormControl(false, Validators.required),
-    phoneNumber: new FormControl('', [Validators.pattern(/^\+?\d{10,15}$/)]),
-    openPositions: new FormControl(0),
-    openPositionsUrl: new FormControl(''),
-  });
-
-  constructor(private companyApi: CompanyApiService) {}
+  ngOnInit(): void {
+    this.initForm();
+  }
 
   onSubmit() {
-    if (this.newCompany.valid) {
-      this.company = this.newCompany.value as CompanyModel;
-      console.log('Form Submitted', this.company);
-      this.companyApi.createCompany(this.company).subscribe({
-        next: response => {
-          alert(`Company "${this.company.name}" created successfully!`);
-          console.log('Company created successfully:', response);
-        },
-        error: error => {
-          console.error('Error creating company:', error);
-          alert('Failed to create company. Please try again.');
-        },
-      });
-      this.newCompany.reset();
-    } else {
+    if (this.newCompany.invalid) {
       alert('Some fields are not correct! Please check again.');
+      return;
     }
+    this.company = this.newCompany.value as CompanyModel;
+    console.log('Form Submitted', this.company);
+    this.companyApi.createCompany(this.company).subscribe({
+      next: response => {
+        alert(`Company "${this.company.name}" created successfully!`);
+        console.log('Company created successfully:', response);
+      },
+      error: error => {
+        console.error('Error creating company:', error);
+        alert('Failed to create company. Please try again.');
+      },
+    });
+    this.newCompany.reset();
   }
 
   isFieldInvalid(field: string) {
-    return this.newCompany.get(field)?.invalid && this.newCompany.get(field)?.touched;
+    const control = this.newCompany.get(field);
+    return !!(control && control.invalid && control.touched);
+  }
+
+  private initForm(): void {
+    this.newCompany = this.fb.group({
+      name: ['', Validators.required],
+      website: ['', [Validators.pattern(/^(https?:\/\/)?[\w-]+(\.[\w-]+)+[/#?]?.*$/)]],
+      vat: ['', Validators.required],
+      logoUrl: [''],
+      address: this.fb.group({
+        country: [''],
+        city: [''],
+        street: [''],
+        streetNumber: [''],
+        postalCode: [''],
+      }),
+      isSponsored: [false, Validators.required],
+      phoneNumber: ['', [Validators.pattern(/^\+?\d{10,15}$/)]],
+      openPositions: [0],
+      openPositionsUrl: [''],
+    });
   }
 }
